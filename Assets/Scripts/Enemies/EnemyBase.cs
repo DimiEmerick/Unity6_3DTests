f9f8311c71330dd06a4a82e9979d078c031dd1f4
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-12-01 16:02:02 -0300

    Trying to fix collision error

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -18,28 +18,19 @@ public class EnemyBase : MonoBehaviour
     {
         Debug.Log("Colidiu com: " + collision.transform.name);
         if (collision.transform.TryGetComponent<PlayerSpeed>(out var player)) player.health.Damage(1);
-        if (collision.transform.CompareTag("GroundCheck"))
-        {
-            health.Damage(1);
-            _player.Jump();
-            if (health.currentLife <= 0) OnKill();
-        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.transform.name + " entrou da área de visão do " + transform.name);
-
-        if (other.CompareTag("Player")) lookAtPlayer = true;
+        if (other.CompareTag("GroundCheck"))
+        {
+            health.Damage(1);
+            _player.Jump();
+            if (health.currentLife <= 0) OnKill();
+        } 
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        Debug.Log(other.transform.name + " saiu da área de visão do " + transform.name);
-        if (other.CompareTag("Player")) lookAtPlayer = false;
-    }
-
-    protected virtual void OnKill()
+    public virtual void OnKill()
     {
         enemyCollider.gameObject.SetActive(false);
     }
@@ -52,7 +43,6 @@ public class EnemyBase : MonoBehaviour
             direction.y = 0;
             Quaternion lookRotation = Quaternion.LookRotation(direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
-            //  transform.LookAt(_player.transform.position);
         }
     }
 }
--- Assets/Scripts/Player/GroundCheck.cs
+++ Assets/Scripts/Player/GroundCheck.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class GroundCheck : MonoBehaviour
 {
-    
 }
