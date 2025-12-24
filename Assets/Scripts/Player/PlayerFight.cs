2b2f55c9b50ed17f171fd4426db0da4b1e785576
Dimi <dimitrios007emerick@gmail.com>
2025-12-23 22:12:12 -0300

    Updated movement

--- Assets/Scripts/Player/PlayerFight.cs
+++ Assets/Scripts/Player/PlayerFight.cs
@@ -2,21 +2,37 @@ using UnityEngine;
 
 public class PlayerFight : MonoBehaviour
 {
+    public Animator animator;
     public CharacterController characterController;
     public float speed;
+    public float turnSpeed;
 
     private void Walk()
     {
-        string direction;
-        if (Input.GetKey(KeyCode.W)) direction = "forward";
-        if (Input.GetKey(KeyCode.A)) direction = "left";
-        if (Input.GetKey(KeyCode.S)) direction = "right";
-        if (Input.GetKey(KeyCode.D)) direction = "back";
+        Vector3 camForward = Camera.main.transform.forward;
+        Vector3 camRight = Camera.main.transform.right;
+        camForward.y = 0f;
+        camRight.y = 0f;
+        camForward.Normalize();
+        camRight.Normalize();
+        Vector3 direction = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)) direction += camForward;
+        if (Input.GetKey(KeyCode.A)) direction -= camRight;
+        if (Input.GetKey(KeyCode.S)) direction -= camForward;
+        if (Input.GetKey(KeyCode.D)) direction += camRight;
+
+        characterController.SimpleMove(direction * Time.deltaTime * speed);
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+
+        if (direction != Vector3.zero) animator.SetBool("Walk", true);
+        else animator.SetBool("Walk", false);
     }
 
     private void Update()
     {
-        if(Input.GetKey(KeyCode.W))
-            characterController.SimpleMove(Vector3.forward * Time.deltaTime * speed);
+        Walk();
     }
 }
