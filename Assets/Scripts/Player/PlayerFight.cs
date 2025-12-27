b419d831daa57b9bc945ed2e27bcd10ce08d728a
Dimi <dimitrios007emerick@gmail.com>
2025-12-26 22:15:45 -0300

    Updated Player basic movement and imported props assets

--- Assets/Scripts/Player/PlayerFight.cs
+++ Assets/Scripts/Player/PlayerFight.cs
@@ -3,43 +3,90 @@ using UnityEngine.InputSystem;
 
 public class PlayerFight : MonoBehaviour
 {
-    public Animator animator;
-    public CharacterController characterController;
-    public float speed;
-    public float turnSpeed;
+    [Header("References")]
+    private CharacterController controller;
+    private Animator animator;
+    [SerializeField] private new Transform camera;
 
-    public InputAction move;
-    public InputAction jump;
+    [Header("Movement Settings")]
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float sprintSpeed = 10f;
+    [SerializeField] private float sprintTransitSpeed = 5f;
+    [SerializeField] private float turnSpeed = 2f;
+    [SerializeField] private float gravity = 9.81f;
+    [SerializeField] private float jumpHeight = 2f;
 
-    private void Walk()
+    private float verticalVelocity;
+    private float speed;
+
+    [Header("Input")]
+    private float moveInput;
+    private float turnInput;
+
+    private void Start()
     {
-        Vector3 camForward = Camera.main.transform.forward;
-        Vector3 camRight = Camera.main.transform.right;
-        camForward.y = 0f;
-        camRight.y = 0f;
-        camForward.Normalize();
-        camRight.Normalize();
-        Vector3 direction = Vector3.zero;
+        controller = GetComponent<CharacterController>();
+        animator = GetComponentInChildren<Animator>();
+    }
 
-        if (Input.GetKey(KeyCode.W)) direction += camForward;
-        if (Input.GetKey(KeyCode.A)) direction -= camRight;
-        if (Input.GetKey(KeyCode.S)) direction -= camForward;
-        if (Input.GetKey(KeyCode.D)) direction += camRight;
+    private void InputManagement()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
 
-        direction = direction.normalized;
-        characterController.SimpleMove(direction * Time.deltaTime * speed);
+    private void GroundMovement()
+    {
+        Vector3 move = new Vector3(turnInput, 0, moveInput);
+        move = camera.transform.TransformDirection(move);
+        if (Input.GetKey(KeyCode.LeftShift)) speed = Mathf.Lerp(speed, sprintSpeed, sprintTransitSpeed * Time.deltaTime);
+        else speed = Mathf.Lerp(speed, walkSpeed, sprintTransitSpeed * Time.deltaTime);
+        move *= speed;
+        move.y = VerticalVelocityCalculation();
+        controller.Move(move * Time.deltaTime);
+    }
 
-        if (direction != Vector3.zero)
+    private void Turn()
+    {
+        if (Mathf.Abs(turnInput) > 0 || Mathf.Abs(moveInput) > 0)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
-            animator.SetBool("Walk", true);
+            Vector3 currentLookDirection = controller.velocity.normalized;
+            currentLookDirection.y = 0;
+            currentLookDirection.Normalize();
+            Quaternion targetRotation = Quaternion.LookRotation(currentLookDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
         }
+    }
+
+    private float VerticalVelocityCalculation()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1f;
+            if (Input.GetButtonDown("Jump")) verticalVelocity = Mathf.Sqrt(jumpHeight * gravity * 2);
+        }
+        else verticalVelocity -= gravity * Time.deltaTime;
+        return verticalVelocity;
+    }
+
+    private void MovementAnimation()
+    {
+        if (Mathf.Abs(turnInput) > 0 || Mathf.Abs(moveInput) > 0) animator.SetBool("Walk", true);
         else animator.SetBool("Walk", false);
+        if (controller.isGrounded) animator.SetTrigger("Land");
+        else if (Input.GetButtonDown("Jump")) animator.SetTrigger("Jump");
+    }
+
+    private void Movement()
+    {
+        GroundMovement();
+        Turn();
+        MovementAnimation();
     }
 
     private void Update()
     {
-        Walk();
+        InputManagement();
+        Movement();
     }
 }
